11ae21223f7c41537e4fb48ff4b10a3dbc158e7a
vinayex2 <vinayex2@gmail.com>
2026-03-12 17:10:00 +0000

    Chat Completion

--- Notes/H.prompt versioning.py
+++ Notes/H.prompt versioning.py
@@ -1,7 +1,12 @@
-%pip install --upgrade "mlflow[databricks]>=3.1.0"
-%restart_python
+# %pip install --upgrade "mlflow[databricks]>=3.1.0"
+# %pip install openai
+# %restart_python
 
 import mlflow
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
 
 # Register a prompt template
 prompt_name = "mlflow_experiments.genai.assistant_prompt"
@@ -23,3 +28,16 @@ mlflow.genai.set_prompt_alias(
 prompt = mlflow.genai.load_prompt(name_or_uri=f"prompts:/{prompt_name}@production")
 # response = llm.invoke(prompt.format(question="How do I reset my password?"))
 print(prompt.format(question="How do I reset my password?"))
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model='gpt-4o-mini',
+    messages = [
+        {'role' :'user',
+         'content': prompt.format(question="How do I reset my password?")
+         }
+    ]
+)
+
+print(response)
